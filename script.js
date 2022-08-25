00e44484f4d1685f973bcd1cf113530f85b6ecf1
conan0220 <100282257+conan0220@users.noreply.github.com>
2022-08-25 22:25:50 +0800

    add script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function copyToClipBoard(text) {
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        console.log("Text copied to clipboard...")
+        alert("Copied!")
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
+}
